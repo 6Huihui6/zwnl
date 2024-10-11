da5cba105eedd5dcc151072767f7df757c80ae38
吴铭辉 <h3103733088@gmail.com>
2024-10-11 20:38:15 +0800

    网关服务配置

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cd /usr/local/src/script || exit 1
-BASE_PATH='/var/lib/jenkins/workspace/zwnl-build'
+BASE_PATH='/var/lib/jenkins/workspace/zwnl-dev-build'
 PROJECT_NAME=""
 PROJECT_PATH=''
 CONTAINER_NAME=""
